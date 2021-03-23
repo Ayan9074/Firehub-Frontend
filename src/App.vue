eb6b742532365fc609fa009399282452c8e66829
Ayan9074 <Ayan9074@yahoo.com>
2021-03-23 13:28:15 +0400

    Homepage css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
+
 	<div class="vue-tempalte">
-		<!-- Navigation -->
+		<!--
+		<Navigation>
 		<header>
 			<nav class="navbar navbar-expand-lg navbar-light changecolor topsize" style="top: 0px; width: 100%; opacity:0.5; position: fixed;" id="nav">
 				<a class="navbar-brand" href="/" style="color: white; z-index: 99 !important; right: 50%; margin-left: 10%; margin-top: 1%;">ZoomClone</a>
@@ -11,8 +13,7 @@
 				</div>
 			</nav>
 		<div class="para" style="position: fixed; left: -30px; top: 0px;"></div>
-		</header>
-
+		</header> -->
 		<!-- Main -->
 		<div class="App">
 			<div class="vertical-center">
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,73 +1,18 @@
 <template>
 	<div class="vue-template">
-        <nav style="position: fixed; left: -40px; top: 25%;">
+        <nav class="menu" tabindex="0">
+            <div class="smartphone-menu-trigger"></div>
+        <header class="avatar">
+                <img src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg" />
+            <h2>A Human</h2>
+        </header>
             <ul>
-                <li>
-                <div class="home-icon">
-                    <div class="roof">
-                    <div class="roof-edge"></div>
-                    </div>
-                    <div class="front"></div>
-                </div>
-                </li>
-                <li>
-                <div class="about-icon">
-                    <div class="head">
-                    <div class="eyes"></div>
-                    <div class="beard"></div>
-                    </div>
-                </div>
-                </li>
-                <li>
-                <div class="work-icon">
-                    <div class="paper"></div>
-                    <div class="lines"></div>
-                    <div class="lines"></div>
-                    <div class="lines"></div>
-                </div>
-                </li>
-                <li>
-                <div class="mail-icon">
-                    <div class="mail-base">
-                    <div class="mail-top"></div>
-                    </div>
-                </div>
-                </li>
-            </ul>
+            <li tabindex="0" class="icon-dashboard"><span>Dashboard</span></li>
+            <li tabindex="0" class="icon-customers"><span>Customers</span></li>
+            <li tabindex="0" class="icon-users"><span>Users</span></li>
+            <li tabindex="0" class="icon-settings"><span>Settings</span></li>
+        </ul>
         </nav>
-        <center>
-        <section style="">
-            <div>
-                <form id="form" action="" style="width: 50%; margin: auto;">
-                    <input id="input" autocomplete="off" /><button>Send</button>
-                </form>
-            </div>
-            <div style="opacity:0.5; padding: 5em; position: fixed; float: right; left: 30em; height: 50em; width: 60em; background-color: black; border-radius: 25px; color: black; top: 7em;">
-                <ul id="messages" style="position: relative;">
-                    <li>hi</li>
-                    <li>hola</li>
-                    <li>hola</li>
-                    <li>hola</li>
-                    <li>hola</li>
-                    <li>hola</li>
-                    <li>hola</li>
-                    <li>bruh</li>
-                    <li>hola</li>
-                    <li>bruh</li>
-                    <li>hola</li>
-                    <li>bruh</li>
-                    <li>hola</li>
-                    <li>bruh</li>
-                    <li>hola</li>
-                    <li>L</li>
-                </ul>
-            </div>
-            <div style="opacity:0.5; position: fixed; float: right; left: 91em; height: 53em; width: 15em; background-color: black; border-radius: 25px; color: red; top: 7em;">
-                hi
-            </div>
-            
-        </section>
-        </center>
 	</div>
 </template>
 <style>
